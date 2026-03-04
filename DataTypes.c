d8c72d14a08b4e6d7ab37fae72bcd2aa2c11a9b8
ankitdev6870 <ankitdev880@gmail.com>
2026-03-04 10:01:59 +0545

    DataTypes
    DataTypes

---
+++ DataTypes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a;
+    // a = 1;
+    int a; 
+    a = 6;
+    
+    // float b = 1.4;
+    float b;
+    b=1.4;
+    
+    // char c = 'a';
+    char c;
+    c='a';
+
+    printf("Output is %d %f %c",a,b,c);
+
+    return 0;
+}
