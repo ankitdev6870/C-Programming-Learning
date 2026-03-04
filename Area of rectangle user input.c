140699449002dd4b12dd0febd7726609206dbad2
ankitdev6870 <ankitdev880@gmail.com>
2026-03-04 10:34:32 +0545

    Area of rectangle user input
    Area of rectangle user input

---
+++ Area of rectangle user input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int l,b,Area;
+printf("Enter length:",l);
+scanf("%d",&l);
+printf("Enter Breadth:",b);
+scanf("%d",&b);
+Area=l*b;
+printf("The area of Rectangle is %d",Area);
+    return 0;
+}
